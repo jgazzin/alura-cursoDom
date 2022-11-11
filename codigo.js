95d0e4596799fe01330b12982dda4b576f999c39
Julieta Gazzin <j.gazzin@gmail.com>
2022-11-11 01:38:55 -0300

    clase 04 completa

--- codigo.js
+++ codigo.js
@@ -1,66 +1,71 @@
-const btn = document.querySelector("[data-form-btn]");
-// variable que toma el selector data-form-btn
+// se mete todo dentro de una variable para que desde el navegador el usuario o tenga acceso al código ( () => { }) y se invoca ()
 
-console.log(btn);
-// variable que contiene la funcion
+( ()=> {
+    const btn = document.querySelector("[data-form-btn]");
+    // variable que toma el selector data-form-btn
+    
+    console.log(btn);
+    // variable que contiene la funcion
+    
+    const createTask = (evento) => {
+        // evita que se recargue la página cada vez q se ejecuta el evento
+        evento.preventDefault();
+        // acción : capturar lo que se escribió en el input
+        const input = document.querySelector("[data-form-input");
+        // guarda contenido del input en una variable
+        const value = input.value;
+        // elemento donde se imprime la respuesta
+        const list = document.querySelector("[data-list]");
+        // crea nuevo elemento:
+        const task = document.createElement("li");
+        // crea clase para el elemento task
+        task.classList.add("card");
+        // borra el input
+        input.value = "";
+    
+        // crea el cógigo que se va a agregar con la tarea
+        const taskContent = document.createElement("div");
+        taskContent.appendChild(checkComplet());
+        const titleTask = document.createElement("span");
+        titleTask.classList.add ("task");
+        titleTask.innerText = value;
+        const contenido = `
+        <i class="fas fa-trash-alt trashIcon icon"></i>`
+        // task.innerHTML=taskContent;
+    
+        // métodos para manipular nodos
+        // agrega el código a cada etiqueta (en el orden que tienen q aparecer)
+        taskContent.appendChild(titleTask);
+        task.appendChild(taskContent);
+        list.appendChild(task);
+        console.log(contenido);
+    
+    };
+    console.log(btn);
+    // (e) => función anonima
+    
+    // listener = evento que inicia la acción elemento.addEventListener
+    btn.addEventListener("click", createTask);
+    // toma el evento y ejecuta la acción
+    
+    // acción del check
+    const checkComplet = () => {
+        const i = document.createElement("i");
+        // le agrega las clases + la etiqueta i
+        i.classList.add("far", "fa-check-square", "icon");
+        i.addEventListener("click", completeTask);
+        // retorna i
+        return i;
+    }
+    
+    const completeTask = (e)=> {
+        // console.log(e.target);
+        const element = e.target;
+        element.classList.toggle("fas");
+        element.classList.toggle("far");
+        element.classList.toggle("completeIcon");
+        // en lugar de poner add y remove - toggle intercala add/ remove según corresponda
+    }
+    // se agrega un evento a la funcion para q devuelva datos del evento y poder capturarlo
+})()
 
-const createTask = (evento) => {
-    // evita que se recargue la página cada vez q se ejecuta el evento
-    evento.preventDefault();
-    // acción : capturar lo que se escribió en el input
-    const input = document.querySelector("[data-form-input");
-    // guarda contenido del input en una variable
-    const value = input.value;
-    // elemento donde se imprime la respuesta
-    const list = document.querySelector("[data-list]");
-    // crea nuevo elemento:
-    const task = document.createElement("li");
-    // crea clase para el elemento task
-    task.classList.add("card");
-    // borra el input
-    input.value = "";
-
-    // crea el cógigo que se va a agregar con la tarea
-    const taskContent = document.createElement("div");
-    taskContent.appendChild(checkComplet());
-    const titleTask = document.createElement("span");
-    titleTask.classList.add ("task");
-    titleTask.innerText = value;
-    const contenido = `
-    <i class="fas fa-trash-alt trashIcon icon"></i>`
-    // task.innerHTML=taskContent;
-
-    // métodos para manipular nodos
-    // agrega el código a cada etiqueta (en el orden que tienen q aparecer)
-    taskContent.appendChild(titleTask);
-    task.appendChild(taskContent);
-    list.appendChild(task);
-    console.log(contenido);
-
-};
-console.log(btn);
-// (e) => función anonima
-
-// listener = evento que inicia la acción elemento.addEventListener
-btn.addEventListener("click", createTask);
-// toma el evento y ejecuta la acción
-
-// acción del check
-const checkComplet = () => {
-    const i = document.createElement("i");
-    // le agrega las clases + la etiqueta i
-    i.classList.add("far", "fa-check-square", "icon");
-    i.addEventListener("click", completeTask);
-    // retorna i
-    return i;
-}
-
-const completeTask = (e)=> {
-    // console.log(e.target);
-    const element = e.target;
-    element.classList.toggle("fas");
-    element.classList.toggle("far");
-    element.classList.toggle("completeIcon");
-    // en lugar de poner add y remove - toggle intercala add/ remove según corresponda
-}
-// se agrega un evento a la funcion para q devuelva datos del evento y poder capturarlo
