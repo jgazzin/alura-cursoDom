37e23ec2ed5684cd30a744c56f5300ebf76d4bd8
Julieta Gazzin <j.gazzin@gmail.com>
2022-11-11 00:42:24 -0300

    parcial- creo 1 nuevo elemento en html

--- codigo.js
+++ codigo.js
@@ -9,7 +9,23 @@ const createTask = (evento) => {
     evento.preventDefault();
     // acción : capturar lo que se escribió en el input
     const input = document.querySelector("[data-form-input");
-    console.log(input.value);
+    // guarda contenido del input en una variable
+    const value = input.value;
+    // capturo el elemnento data-task (lista)
+    const task = document.querySelector("[data-task]");
+    // borra el input
+    input.value = "";
+    // variable donde se guarda código nuevo q se va a agregar al html
+    const contenido = `<div>
+    <i class="far fa-check-square icon">
+    </i>
+    <span class="task">${value}</span>
+    </div>`
+    // en ${value} agrega el texto que se había tomado del input
+    // agrega contenido en task para q se imprima en pantalla
+    task.innerHTML=contenido;
+    console.log(task);
+
 };
 // (e) => función anonima
 
