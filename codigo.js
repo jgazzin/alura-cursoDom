7d995c5834bd28952dd0731fe8fd495b45b93ab3
Julieta Gazzin <j.gazzin@gmail.com>
2022-11-12 17:00:24 -0300

    clase03 terminada - maquitando tareas previas

--- codigo.js
+++ codigo.js
@@ -6,75 +6,96 @@
     
     // variable que contiene la funcion
     const addTask = (evento) => {
-        // elemento donde se imprime la respuesta
-        const list = document.querySelector("[data-list]");
-        const task = createTask(evento);
-        // agreta a la <ul> las li nuevas con cada tarea
-        list.appendChild(task);
-    }
-
-    const createTask = (evento) => {
         // evita que se recargue la página cada vez q se ejecuta el evento
         evento.preventDefault();
-        // arreglo para almacenamieto de task en local
-        // se inicia directamente con lo q ya haya almacenado en local
-        const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
 
+        // elemento donde se imprime la respuesta
+        const list = document.querySelector("[data-list]");
         // acción : capturar lo que se escribió en el input
         const input = document.querySelector("[data-form-input");
         const calendar = document.querySelector("[data-form-date");
+        
         // guarda contenido del input en una variable
         const value = input.value;
         const date = calendar.value;
         const dateFormat = moment(date).format("DD/MM/YYYY");
         console.log(dateFormat);
 
-        // crea nuevo elemento:
-        const task = document.createElement("li");
-        // crea clase para el elemento task
-        task.classList.add("card");
-        // borra el input
-        input.value = "";
-    
-        // crea el cógigo que se va a agregar con la tarea
-        const taskContent = document.createElement("div");
-        taskContent.appendChild(checkComplet());
-
         // almacenamieto de datos en un objeto
         const taskObj = {
             value,
             dateFormat
         };
-        // agregar al arreglo taskList
-        taskList.push(taskObj);
-        localStorage.setItem("tasks", JSON.stringify(taskList));       
+
+        // arreglo para almacenamieto de task en local
+        // se inicia directamente con lo q ya haya almacenado en local
+        const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+        // agregar los atributos del arreglo a taskList
+        taskList.push({value, dateFormat});
+        localStorage.setItem("tasks", JSON.stringify(taskList));
+
+        const task = createTask(taskObj);
+        // agreta a la <ul> las li nuevas con cada tarea
+        list.appendChild(task);
+
+        // borra el input
+        input.value = "";
+        calendar.value = "";
+    }
+
+    // leer los datos de local para crear tareas previas
+    const readTask = () => {
+        const list = document.querySelector("[data-list");
+        const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+
+        // recorre cada elemento guardado en arreglo local
+        taskList.forEach((task) => {
+            // crea el codigo para cada elemneto
+            console.log(createTask(task));
+            // envía el código a la etiqueta que crea createTask
+            list.appendChild(createTask(task));
+
+        });
+    }
+
+    function createTask({ value, dateFormat }) {
+        // crea nuevo elemento:
+        const task = document.createElement("li");
+            // crea clase para el elemento task
+            task.classList.add("card");
+
+        // crea el cógigo que se va a agregar con la tarea
+        const taskContent = document.createElement("div");
+            taskContent.appendChild(checkComplet());
 
         const titleTask = document.createElement("span");
-        titleTask.classList.add ("task");
-        titleTask.innerText = value;
-    
-        // métodos para manipular nodos
-        // agrega el código a cada etiqueta (en el orden que tienen q aparecer)
-        taskContent.appendChild(titleTask);
+            titleTask.classList.add("task");
+            titleTask.innerText = value;
+
+            // métodos para manipular nodos
+            // agrega el código a cada etiqueta (en el orden que tienen q aparecer)
+            taskContent.appendChild(titleTask);
 
         // crea elemento de impresió de fecha
-        const dateElement =document.createElement("span");
-        dateElement.innerHTML = dateFormat;
-        
-        task.appendChild(taskContent);
-        task.appendChild(dateElement);
-        task.appendChild(borrarIcon());
+        const dateElement = document.createElement("span");
+            dateElement.innerHTML = dateFormat;
+
+            task.appendChild(taskContent);
+            task.appendChild(dateElement);
+            task.appendChild(borrarIcon());
 
         // envía la tarea creada a addTask
         return task;
-    };
+    }
+
     console.log(btn);
     // (e) => función anonima
     
     // listener = evento que inicia la acción elemento.addEventListener
     btn.addEventListener("click", addTask);
     // toma el evento y ejecuta la acción
-    
+
+    // --- acciones con las tareas ---
     // acción del check
     const checkComplet = () => {
         const i = document.createElement("i");
@@ -109,5 +130,8 @@
         const tagPadre = e.target.parentElement; 
         tagPadre.remove();
     }
+
+    // lee datos almacenados en local
+    readTask();
 })()
 
